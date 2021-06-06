c99ae5384a2772b623a2cd0f4212e814b65a86d0
mboxjem <mboxjem@gmail.com>
2021-06-07 01:57:10 +0700

    1.6

--- badvpn/badvpn.sh
+++ badvpn/badvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2 | tar -C /tmp/ -xj
+curl -s https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2 | tar xvfj - -C /tmp/
 mkdir /tmp/badvpn-1.999.128/build
 cmake /tmp/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1
 make install
